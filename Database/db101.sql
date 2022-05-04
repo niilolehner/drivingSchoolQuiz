ebc69a34e1e8bcd05771aedb2efe2164f7079571
Niilo Lehner <niilo.lehner@gmail.com>
2022-05-04 14:34:51 +0300

    Updated DB with more questions, remember to drop old DB and import new in your phpMyadmin

--- Database/db101.sql
+++ Database/db101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2022 at 12:10 PM
+-- Generation Time: May 04, 2022 at 01:32 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -76,7 +76,19 @@ CREATE TABLE `quizqa` (
 INSERT INTO `quizqa` (`QaID`, `Question`, `RightAnswer`, `WrongAnswer1`, `WrongAnswer2`) VALUES
 (1, 'Missä sijaitsee kytkin?', 'Vasen', 'Oikea', 'Keskellä'),
 (2, 'Minkä on oikea toimintajärjestys?', 'Vilkku-Vilkaisu olan yli-Kaistanvaihto', 'Vilkku-Kaistanvaihto-Olan yli katse', 'Kaistanvaihto-Vilkku-Olan yli vilkaisu'),
-(3, 'Mikä on suurin sallittu nopeus Suomessa?', '120km/h', '80km/h', '100km/h');
+(3, 'Mikä on suurin sallittu nopeus Suomessa?', '120km/h', '80km/h', '100km/h'),
+(4, 'Näet jalankulkijan koiran kanssa, jolla on keltainen tai viininpunainen takki. Mitä tämä osoittaa? Jalankulkija on:', 'Kuuro ihminen', 'Vanha ihminen', 'Koiran kouluttaja'),
+(5, 'Missä näistä tilanteista sinun tulee välttää ohittamista?', 'Kun lähestyn tien pudotusta', 'Heti tien mutkan jälkeen', '50km/h tiellä'),
+(6, 'Kuljet 80 km/h hyvällä, kuivalla tiellä. Mikä on tyypillinen kokonaispysähdysmatkasi?', '53 metriä', '36 metriä', '75 metriä'),
+(7, 'Mikä väri seuraa vihreää risteyksessä?', 'Keltainen', 'Punainen', 'Vilkkuva punainen'),
+(8, 'Kuinka monta prosenttia tieliikenteen päästöistä on?', '20%', '10%', '40%'),
+(9, 'Hinaat pientä perävaunua vilkkaalla kolmikaistaisella moottoritiellä. Kaikki kaistat ovat auki. Sinun täytyy:', 'Käytä vain oikeaa ja keskikaistaa', 'Ei saa ylittää 80km/h', 'Älä ohita'),
+(10, 'Onnettomuustilanteessa on tärkeää huolehtia mahdollisista uhreista. Kun alue on turvallinen, sinun tulee:', 'Pidä ne ajoneuvonsa sisällä', 'Ota heidät pois ajoneuvostaan', 'Anna heille juotavaa'),
+(11, 'Odotat kääntymistä pieneltä tieltä. Iso ajoneuvo lähestyy vasemmalta. Sinulla on aikaa kääntyä, mutta sinun tulee odottaa. Miksi?', 'Suuri ajoneuvo voi helposti piilottaa ohittavan ajoneuvon', 'Suuri ajoneuvo voi kääntyä äkillisesti', 'Isoa ajoneuvoa on vaikea ohjata suoraan'),
+(12, 'Milloin voit ohittaa toisen ajoneuvon oikealta?', 'Kun olet yksisuuntaisella kadulla', 'Kun edessä oleva ajoneuvo antaa merkin kääntyä vasemmalle', 'Kun hitaampi ajoneuvo kulkee kaksiajoradan vasemmalla kaistalla'),
+(13, 'Valitset väärän reitin ja huomaat olevasi yksisuuntaisella kadulla. Mitä sinun pitäisi tehdä?', 'Jatka tien päähän', 'Käänny ympäri', 'Peruuttaa'),
+(14, 'Ajat alas pitkää, jyrkkää mäkeä. Huomaat yhtäkkiä, että jarrut eivät toimi normaalisti. Mikä on tavallinen syy tähän?', 'Jarrujen ylikuumeneminen', 'Hidas kuljettajan reaktioaika', 'Huono jarrut'),
+(15, 'Mitä sinun tulee tehdä risteyksessä, kun keltainen valo vilkkuu?', 'Valmistautua pysähtymään', 'Vaihda kaistaa', 'Koventaa vauhtia');
 
 -- --------------------------------------------------------
 
@@ -156,7 +168,7 @@ ALTER TABLE `achievements`
 -- AUTO_INCREMENT for table `quizqa`
 --
 ALTER TABLE `quizqa`
-  MODIFY `QaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `QaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `scoreboard`
