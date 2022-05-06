59fe6dd07ba85a8c1eab78e19ac7b31f221213c4
MallaMalla <79974713+MallaMalla@users.noreply.github.com>
2022-05-06 11:43:29 +0300

    sdf

--- DatadriversOy/index.php
+++ DatadriversOy/index.php
@@ -1,5 +1,5 @@
 <?php
-    require 'includes/header.php';
+require 'includes/header.php';
 
 
 //Context comes here
@@ -10,7 +10,7 @@ if ($page === 'quiz') {
 }
 
 
-    require 'includes/footer.php';
+require 'includes/footer.php';
 ?>
 
 
