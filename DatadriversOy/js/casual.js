c939e85b4eef7a92366b36b62a01ba179e1bac11
MallaMalla <79974713+MallaMalla@users.noreply.github.com>
2022-05-09 12:34:08 +0300

    question change, but only once

--- DatadriversOy/js/casual.js
+++ DatadriversOy/js/casual.js
@@ -4,5 +4,17 @@ const vastaus2 = document.getElementById("vastaus2");
 const vastaus3 = document.getElementById("vastaus3");
 
 
+    function changeQuestion() {
 
+        let question = shuffledQuestionArray[0].Question;
+        document.getElementById("kysymys").innerHTML = question;
 
+        let ans1 = shuffledQuestionArray[0].RightAnswer;
+        document.getElementById("vastaus1").innerHTML = ans1;
+
+        let ans2 = shuffledQuestionArray[0].WrongAnswer1;
+        document.getElementById("vastaus2").innerHTML = ans2;
+
+        let ans3 = shuffledQuestionArray[0].WrongAnswer2;
+        document.getElementById("vastaus3").innerHTML = ans3;
+    }
