c384f5c1fa1850c0632a362c4fca17c9cdddee93
Aesmou <ismo.manninen@edu.lapinamk.fi>
2022-05-09 18:42:25 +0300

    Created teacher-view

--- DatadriversOy/includes/header.php
+++ DatadriversOy/includes/header.php
@@ -9,7 +9,7 @@
 
         <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer></script>
     <script src="https://kit.fontawesome.com/416b989c77.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
     <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
@@ -27,46 +27,50 @@
     <link href="css/styles.css" rel="stylesheet" />
         
     <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
     <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
     <!-- * *                               SB Forms JS                               * *-->
     <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
     <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script> 
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
     <script src="./js/index.js"></script>
-    
+
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
     <title>Datadrivers Oy</title>
 </head>
 
+<body id="page-top">
+
 <?php
+    isset($_GET['page']) ? $page = $_GET['page'] : $page = '';
 
-isset($_GET['page']) ? $page = $_GET['page'] : $page = '';
+    if ($page === ''){ 
+        $page = 'userChoose';
+    }
 
-if ($page === ''){ 
-    $page = 'userChoose';
-}
-
-if ($page !== 'welcome' && $page !== 'userChoose') {
+    if ($page !== 'welcome' && $page !== 'userChoose') {
 ?>
-    <body id="page-top">
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="index.php?page=userChoose">Valitse käyttäjä (demo)</a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Valikko
-                <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="">Harjoittelutehtäviä</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">Teoriakoeharjoitus</a></li>
-                    <li class="nav-item"><a class="nav-link" href="">Palkinnot</a></li>
-                </ul>
-            </div>
+
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container px-4 px-lg-5">
+        <a class="navbar-brand" href="index.php?page=userChoose">Valitse käyttäjä (demo)</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Valikko
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="">Harjoittelutehtäviä</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Teoriakoeharjoitus</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Palkinnot</a></li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 <?php
 }
 ?>
