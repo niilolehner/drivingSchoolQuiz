f3d2b982d95feb5b95798f93192baacb4188bb48
Niilo Lehner <niilo.lehner@gmail.com>
2022-05-11 17:56:23 +0300

    added modeInput to checkAndAwardAchievs, further worked on the achiev unlock conditions

--- DatadriversOy/js/achievements.js
+++ DatadriversOy/js/achievements.js
@@ -7,26 +7,29 @@ function listUnlockedAchievements() {
   document.getElementById('unlockedAchievsList').innerHTML +='<img class="miniachiev" src="images/' + `${AchievementID}` + '.png">' + ' ' + `${POPtext}`});
 }
 
-listUnlockedAchievements();
-
-// check unlockedAchievsArray if achiev present, needs AchievementID as a string
+// check unlockedAchievsArray if achiev present, needs AchievementID (for example "1") as a string
 function isAchievUnlocked(unlockedAchievsArray, AchievementID) {
   return unlockedAchievsArray.some(function(el) {
     return el.AchievementID === AchievementID;
   }); 
 }
+
 // check that the achievements are not already unlocked
 // also check if requirements of locked achievements are satisfied for unlock
 // unlock the achievements
 // trigger achievements animation
-function checkAndAwardAchievs(timeInput, scoreInput, streakInput) {
+//
+// modeInput is "admin", "competitive" or "casual" as string, timeInput is minutes as integer
+// scoreInput is correct answers given as integer, streakInput is correct answer streak as integer
+function checkAndAwardAchievs(modeInput, timeInput, scoreInput, streakInput) {
+  let mode = modeInput;
   let time = timeInput;
   let score = scoreInput;
   let streak = streakInput;
-  // the 'students' table entry for QuizesDone needs to be updated before checkAndAwardAchievs is fired
+  // the 'students' table entry for QuizesDone needs to be updated on quiz completion, before checkAndAwardAchievs is fired
   let studentInfo = arrayFromPHP("students");
   let quizesCompleted = Number(studentInfo[0].QuizesDone);
-  let keksi = ""; // substitute ""- with studentInfo[0].Keksi as soon as Keksi is in the DB
+  let keksi = studentInfo[0].Keksi;
   let achievementsInfo = arrayFromPHP("achievements");
   let unlockedAchievsArray = arrayFromPHP("studentachievements");
   let numberOfAchievs = unlockedAchievsArray.length;
@@ -63,23 +66,25 @@ function checkAndAwardAchievs(timeInput, scoreInput, streakInput) {
   if (isAchievUnlocked(unlockedAchievsArray, '11') === false && quizesCompleted >= 200) {
 
   }
-  if (isAchievUnlocked(unlockedAchievsArray, '12') === false && time <= 5) {
+  if (isAchievUnlocked(unlockedAchievsArray, '12') === false && mode === "competitive" && time <= 5 && score === 10) {
 
   }
-  if (isAchievUnlocked(unlockedAchievsArray, '13') === false && time >= 15) {
+  if (isAchievUnlocked(unlockedAchievsArray, '13') === false && mode === "competitive" && time >= 15 && score === 10) {
 
   }
-  if (isAchievUnlocked(unlockedAchievsArray, '14') === false && score === 0) {
+  if (isAchievUnlocked(unlockedAchievsArray, '14') === false && mode === "competitive" && score === 0) {
 
   }
-  if (isAchievUnlocked(unlockedAchievsArray, '15') === false && keksi === true) {
+  if (isAchievUnlocked(unlockedAchievsArray, '15') === false && keksi === "1") {
 
   }
   if (isAchievUnlocked(unlockedAchievsArray, '16') === false && numberOfAchievs === 15) {
-
+    
   }
 
 
 }
 
-checkAndAwardAchievs(6, 0, 60);
+checkAndAwardAchievs("admin", 0, 0, 0);
+
+listUnlockedAchievements();
