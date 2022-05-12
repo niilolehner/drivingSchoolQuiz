5034e662308a9a282321d78e3a0544cbaf12c38f
Niilo Lehner <niilo.lehner@gmail.com>
2022-05-12 07:41:56 +0300

    working on writeToDatabase.php features, adding prepared statements

--- DatadriversOy/php/writeToDatabase.php
+++ DatadriversOy/php/writeToDatabase.php
@@ -1,13 +1,35 @@
-<?php 
-
-// dummy code to test passing variables from JS to PHP through JSON
+<?php
+isset($_GET['page']) ? $page = $_GET['page'] : $page = '';
+isset($_GET['studentId']) ? $studentId = $_GET['studentId'] : $studentId = '';
 
 include 'dbConnection.php';
 
+$sql = '';
+$rows = [];
+
+$studentId = 1; //testing purpose
+
+//Get data in JSON-format and insert/update into database through prepared statements:
 $requestPayload = file_get_contents("php://input");
 
 $phpArray = json_decode($requestPayload, true);
 
-var_dump($phpArray);
+if ($page === 'students') {
+  $stmt = $conn->prepare("INSERT INTO $page (example1, example2, example3) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $example1 , $example2, $example3);
+  $stmt->execute();
+}
+else if ($page === 'scoreboard') {
+  $stmt = $conn->prepare("INSERT INTO $page (example1, example2, example3) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $example1 , $example2, $example3);
+  $stmt->execute();
+}
+else if ($page === 'studentachievements') {
+  $stmt = $conn->prepare("INSERT INTO $page (example1, example2, example3) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $example1 , $example2, $example3);
+  $stmt->execute();
+}
+
+mysqli_close($conn);
 
 ?>
