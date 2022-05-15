b452a683893b5e83988ab841f6c8e2be11345838
Niilo Lehner <niilo.lehner@gmail.com>
2022-05-15 23:16:04 +0300

    Quick fix

--- DatadriversOy/js/achievements.js
+++ DatadriversOy/js/achievements.js
@@ -11,7 +11,7 @@ function listUnlockedAchievements() {
   }  
 }
 
-// fires main achievements function in admin mode to check and award achievements when achievements.html is loaded (see $studentId on getDatabaseData.php)
+// fires main achievements function in admin mode to check and award achievements when achievements.js is loaded (see $studentId on getDatabaseData.php)
 checkAndAwardAchievs("admin", 0, 0, 0);
 
 // fires the function that populates unlocked achievements on the achievements.html for the currently selected student (see $studentId on getDatabaseData.php)
