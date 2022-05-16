3375f406a5247a8dccac448143ef9240474cedfb
MallaMalla <79974713+MallaMalla@users.noreply.github.com>
2022-05-16 08:44:44 +0300

    personal best update, and competitive fix

--- DatadriversOy/js/competitive.js
+++ DatadriversOy/js/competitive.js
@@ -44,6 +44,20 @@ var newdate = year + "-" + month + "-" + day;
     let ctotalScore = 0;
 
 
+     function shuffleArray(array) {
+            for (var i = array.length - 1; i > 0; i--) {
+ 
+                // Generate random number
+                var j = Math.floor(Math.random() * (i + 1));
+ 
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+ 
+            return array;
+}
+    
 function startFastQuiz() {
             ckysymysTxt.style.visibility = 'hidden';
             cvastaus1Btn.style.visibility = 'hidden';
@@ -172,6 +186,7 @@ function startFastQuiz() {
             QuizesDone: oldQuizesDone,
             StudentID: 1
         }
+        console.log(newBestForDatabase);
         arrayToPHP(newBestForDatabase, "personalBestToUpdate");
     }
 }
